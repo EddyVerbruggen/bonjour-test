edb0b7a9c39e012bde1f957634aeaf6bc1418308
EddyVerbruggen <eddyverbruggen@gmail.com>
2018-02-07 21:01:14 +0100

    Extend `NSObject`

--- app/providers/bonjour.ios.ts
+++ app/providers/bonjour.ios.ts
@@ -4,16 +4,24 @@ import { BonjourCommonProvider } from "./bonjour.common";
 /* Define NSNetServiceBrowserDelegate implementation class */
 
 // class MyNSNetServiceBrowserDelegate extends NSObject implements NSNetServiceBrowserDelegate {
-class MyNSNetServiceBrowserDelegate extends NSObjectProtocol implements NSNetServiceBrowserDelegate {
-  // public static ObjCProtocols = [NSNetServiceBrowserDelegate];
+class MyNSNetServiceBrowserDelegate extends NSObject implements NSNetServiceBrowserDelegate {
+  public static ObjCProtocols = [NSNetServiceBrowserDelegate];
 
-  // static new(): MyNSNetServiceBrowserDelegate {
-  //   return <MyNSNetServiceBrowserDelegate>super.new();
-  // }
+  static new(): MyNSNetServiceBrowserDelegate {
+    return <MyNSNetServiceBrowserDelegate>super.new();
+  }
+
+  private _callback: (result: string) => void;
+
+  public initWithCallback(callback: (result: string) => void): MyNSNetServiceBrowserDelegate {
+    this._callback = callback;
+    return this;
+  }
 
   public netServiceBrowserDidFindDomainMoreComing(browser: NSNetServiceBrowser, domainString: string, moreComing: boolean) {
     console.log(`netServiceBrowserDidFindDomainMoreComing: ${domainString}`);
     console.log(`moreComing: ${moreComing}`);
+    this._callback(domainString);
   }
 
   public netServiceBrowserWillSearch(browser:NSNetServiceBrowser) {
@@ -43,7 +51,7 @@ export class BonjourProvider extends BonjourCommonProvider {
   public startDeviceDiscovery() {
     return new Promise((resolve, reject) => {
 
-      this.netServiceBrowser.delegate = MyNSNetServiceBrowserDelegate;
+      this.netServiceBrowser.delegate = MyNSNetServiceBrowserDelegate.new().initWithCallback(result => console.log(result));
       // this.netServiceBrowser.searchForRegistrationDomains();
       this.netServiceBrowser.searchForServicesOfTypeInDomain('_http._tcp', '');
 
